3a542b97cbff44cbad9e6ac82cdd571a7f998448
harsha136 <sgandi@gitam.in>
2024-07-11 21:53:55 +0530

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def quadratic_model(time):
+    a = 0.1
+    b = -1
+    c = 30
+
+    temperature = a*(time**2) + b*time + c
+    return temperature
+
+def main():
+    time_values = np.linspace(0, 10, 50)
+
+    temperature_values = quadratic_model(time_values)
+    plt.plot(time_values,temperature_values,label='Hard Coded Coefficient')
+    plt.xlabel('Time')
+    plt.ylabel('Temperature')
+    plt.legend()
+    plt.title('Quadratic equation with hard coded coefficient')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
