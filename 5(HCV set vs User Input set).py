f27f1b479ca1e18f102fcfa7de7751a17d544839
harsha1821 <sgandi@gitam.in>
2024-07-12 14:45:38 +0530

    Update and rename 5.py to 5(HCV set vs User Input set).py

--- 5(HCV set vs User Input set).py
+++ 5(HCV set vs User Input set).py
@@ -8,19 +8,19 @@ def quadratic_model(time, a, b, c):
 def main():
     time_values = np.linspace(0, 10, 50)
 
-    a_hc = 15
-    b_hc = 1
-    c_hc = 30
+    a_hc = 22
+    b_hc = -8
+    c_hc = 18
 
-    a_user = float(input("Enter the value for coefficient a: "))
-    b_user = float(input("Enter the value for coefficient b: "))
-    c_user = float(input("Enter the value for coefficient c: "))
+    a_user = float(input("Enter the value for coeff a: "))
+    b_user = float(input("Enter the value for coeff b: "))
+    c_user = float(input("Enter the value for coeff c: "))
 
     temperature_hard = quadratic_model(time_values, a_hc, b_hc, c_hc)
     plt.plot(time_values, temperature_hard, label=f'Hard Coded Coefficient (a={a_hc}, b={b_hc}, c={c_hc})')
 
     temperature_user = quadratic_model(time_values, a_user, b_user, c_user)
-    plt.plot(time_values, temperature_user, label=f'User INput COefficient (a={a_user}, b={b_user}, c={c_user})')
+    plt.plot(time_values, temperature_user, label=f'User Input COefficient (a={a_user}, b={b_user}, c={c_user})')
 
     plt.xlabel('Time')
     plt.ylabel('Temperature')
